1ae873c349d7ddaf0464e83fdb790ddf326f8245
Carter Close <closecw@gmail.com>
2025-04-28 17:20:46 -0500

    Add Project3 file

---
+++ Project3.cpp
@@ -0,0 +1,15 @@
+/**
+ * Stable Matching project
+ * Uses the Gale-Shapley algorithm to find stable matches of employers -> employees
+ * @author Carter Close
+ * @version 1.0
+ */
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+int main()
+{
+    return 0;
+}
